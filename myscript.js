b9c8e9588b6cf0f0a649a01857145ef535c8c988
Rafails <john_qr@o2.pl>
2016-03-09 23:43:07 +0100

    some fun with the smallest number :)

--- myscript.js
+++ myscript.js
@@ -15,3 +15,18 @@ var komuchy = ["wałęsa śmieć", "kwaśniewski", "kiszczak"];
 document.getElementById("demo2").innerHTML = komuchy.toString() + " " + "to sa komuchy";
 var numbers = [1,2,3,4,5,6,7,8,9]
 document.getElementById('demo3').innerHTML = numbers.join(" * ") + " = WIELE"
+
+function butFunction()
+{
+	var message = "";
+	var number = document.getElementById("in1");
+	if (number.validity.rangeUnderflow)
+		{
+		message = "TOO SMALL!!!!!!!! MORE!!!!!!";
+		}
+	else 
+		{
+		message = "OKAY, THIS IS GOOD";
+		}
+	document.getElementById("demo5").innerHTML = message;
+}
